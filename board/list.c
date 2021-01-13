fb2048a4cacecbaa15c8bcfbe32cb6533a7e9dea
MaciejWitkowskiDev <mwit229@gmail.com>
2021-01-13 09:52:55 +0100

    Implemented a list of rooms

---
+++ board/list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "list.h"
+
+/*
+List handbook:
+(*list) to zawsze ostatni element listy
+(*list)->next to zawsze pierwszy element listy
+*/
+
+//may be of use in the future?
+List create_list(){
+    return NULL;
+}
+
+bool list_empty(List *list){
+    return *list == NULL;
+}
+
+void push(List *list, Room *room){
+    Node *new_node = malloc(sizeof(Node)+1);
+    new_node->data = room;
+    if(list_empty(list)){
+        new_node->next = new_node;
+        *list = new_node;
+        return;
+    }
+    new_node->next = (*list)->next;
+    (*list)->next = new_node;
+}
+
+Room* pop(List *list){
+    if(list_empty(list)){
+        return NULL;
+    }
+    Node *first = (*list)->next;
+    Room* room = first->data;
+    (*list)->next = first->next;
+    free(first);
+    return room;
+}
--- board/list.h
+++ board/list.h
@@ -1,12 +1,16 @@
 #include "room.h"
 
 typedef struct node {
-    Room data;
-    Room* next;
+    Room* data;
+    struct node* next;
 } Node; 
 
 typedef Node* List;
 
+List create_list();
+
 void push(List *list, Room *room);
 
 Room* pop();
+
+bool list_empty(List *list)
