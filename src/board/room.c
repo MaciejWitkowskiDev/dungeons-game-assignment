a1600c0095c2e49283fdcbc0c56c7082d97cd02d
MaciejWitkowskiDev <mwit229@gmail.com>
2021-01-15 19:01:35 +0100

    added the create room function

--- src/board/room.c
+++ src/board/room.c
@@ -1,2 +1,12 @@
+#include <stdlib.h>
 #include <stdbool.h>
 #include "room.h"
+
+Room* create_room(short int room_type, Room* prev, Room* left, Room* right){
+    Room* new_room = malloc(sizeof(Room)+1);
+    new_room->room_type = room_type;
+    new_room->prev = prev;
+    new_room->left = left;
+    new_room->right = right;
+    return new_room;
+}
--- src/board/room.h
+++ src/board/room.h
@@ -1,9 +1,12 @@
+
+
 //The room-node:
 typedef struct room{
-    enum type{root, intermed, terminary} room_type;
+    short int room_type;
     struct room* prev;
     struct room* left;
     struct room* right;
     //Room properties will go here
 } Room;
 
+Room* create_room(short int room_type, Room* prev, Room* left, Room* right);
