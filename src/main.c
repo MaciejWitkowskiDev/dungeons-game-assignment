5fec6aea5571344e924472f0e4e275f1933aba42
MaciejWitkowskiDev <mwit229@gmail.com>
2021-01-28 15:08:04 +0100

    setting the clearing function based on os

--- src/main.c
+++ src/main.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <ncurses.h>
 #include "./player/player.h"
+
+#ifdef _WIN32
+	    void clear_screen(){
+            system("cls");
+        }
+	#elif __linux__
+	    void clear_screen(){
+            system("clear");
+        }
+	#elif __unix__
+	    void clear_screen(){
+            system("clear");
+        }
+	#else
+        void clear_screen(){
+           printf("Wyglada na to ze uzywasz niewspieranego systemu!!\n");
+           printf("Nie wszystkie funkcje programu beda dzialac poprawnie!\n");
+           return 0;
+        }
+	    
+	#endif
 
 
 int main(){
+    clear_screen();
     time_t seed = time(NULL);
     Board game_board = generate_board(5, seed);
     Player *player_1 = malloc(sizeof(Player)+1);
@@ -20,6 +41,7 @@ int main(){
     populate_board(&game_board,seed);
     player_1->current_room = game_board;
     player_2->current_room = game_board;
+    clear_screen();
     
     return 0;
     
